937b78b28fa286d3b3c1cd51f8cfa74badedf9bd
Flo <flocela@gmail.com>
2021-08-17 17:23:14 +0000

    Added tests for Resdient_StepDown

--- tests/test_Resident_StepUp.cpp
+++ tests/test_Resident_StepUp.cpp
@@ -1,80 +1,36 @@
 #include "catch.hpp"
 #include "../src/Resident_StepUp.h"
+#include "../src/Resident_Flat.h"
 using Catch::Matchers::Contains;
 
 
-Resident_StepUp blNeighbor01{
-    1,
-    Color::blue, // Only argument that matters.
-    0.0,  
-    0.25, 
-    0.25, 
-    0.75, 
-    0.5   
-};
+// Neighbors are used in getHappiness() method.
+// Only arugment that matteres is Color argument.
+Resident_Flat blNeighbor01{1, Color::blue, 0.0, 0.0, 0.0 };
 
-Resident_StepUp blNeighbor02{
-    2,
-    Color::blue, // Only argument that matters.
-    0.0,  
-    0.25, 
-    0.25, 
-    0.75, 
-    0.5   
-};
+Resident_Flat blNeighbor02{2, Color::blue, 0.0, 0.0, 0.0 };
 
-Resident_StepUp blNeighbor03{
-    3,
-    Color::blue, // Only argument that matters.
-    0.0,  
-    0.25, 
-    0.25, 
-    0.75, 
-    0.5   
-};
+Resident_Flat blNeighbor03{3, Color::blue, 0.0, 0.0, 0.0 };
     
-Resident_StepUp grNeighbor01{
-    11,
-    Color::green, // Only argument that matters.
-    0.0,  
-    0.25, 
-    0.25, 
-    0.75, 
-    0.5   
-};
+Resident_Flat grNeighbor01{1, Color::green, 0.0, 0.0, 0.0 };
 
-Resident_StepUp grNeighbor02{
-    12,
-    Color::green, // Only argument that matters.
-    0.0,  
-    0.25, 
-    0.25, 
-    0.75, 
-    0.5   
-};
+Resident_Flat grNeighbor02{2, Color::green, 0.0, 0.0, 0.0 };
 
-Resident_StepUp grNeighbor03{
-    13,
-    Color::green, // Only argument that matters.
-    0.0,  
-    0.25, 
-    0.25, 
-    0.75, 
-    0.5   
-};
+Resident_Flat grNeighbor03{3, Color::green, 0.0, 0.0, 0.0 };
     
 
 TEST_CASE("Resident_StepUp Ctor throws exception if happinessGoal is greater than 1.0.")
 {   
     REQUIRE_THROWS_WITH(
         Resident_StepUp(
-            1, 
+            0, 
             Color::green, 
-            5.0, // allowedMovementDistance
+            0,
             1.2, // happinessGoal
-            0.0, // happinessValueAtZeroDiversity
-            1.0, // diversityWhereDropHappens
-            0.5 ),
+            0.0,
+            .1,
+            0.2
+        ),
         Contains("must be between 0.0 and 1.0 inclusive") 
     );
 
@@ -83,85 +39,113 @@ TEST_CASE("Resident_StepUp Ctor throws exception if happinessGoal is greater tha
 TEST_CASE("Resident_StepUp Ctor throws exception if happinessGoal is less than 0.0")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          -0.25,
-                          0.0,
-                          1.0,
-                          0.5 ), Contains("must be between 0.0 and 1.0 inclusive") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            -.1, // happinessGoal
+            0.1,
+            0.2,
+            0
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
-TEST_CASE("Resident_StepUp Ctor throws exception if happinessValue at zero is less than zero.")
+TEST_CASE("Resident_StepUp Ctor throws exception if happinessValue at zero is less than 0.0.")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          -0.3,
-                          0.0,
-                          0.5 ), Contains("must be between 0.0 and 1.0 inclusive") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            -.1, // happinessValueAtZeroDiversity 
+            .1,
+            0
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
 TEST_CASE("Resident_StepUp Ctor throws exception if happinessValue at zero is greater than 1.0.")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          1.2,
-                          0.0,
-                          0.5 ), Contains("must be between 0.0 and 1.0 inclusive") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            1.2, // happinessValueAtZeroDiversity 
+            .1,
+            0
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
-TEST_CASE("Resident_StepUp Ctor throws exception if happinessValue at one is less than zero.")
+TEST_CASE("Resident_StepUp Ctor throws exception if happinessValue at one is less than 0.0.")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          0.3,
-                          -0.3,
-                          0.5 ), Contains("must be between 0.0 and 1.0 inclusive") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            0.2,  
+            -.1, // happinessValueAtOneDiversity
+            0
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
 TEST_CASE("Resident_StepUp Ctor throws exception if happinessValue at one is greater than 1.0.")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          .5,
-                          1.2,
-                          0.5 ), Contains("must be between 0.0 and 1.0 inclusive") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            0.2,  
+            1.1, // happinessValueAtOneDiversity
+            0
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
 TEST_CASE("Resident_StepUp Ctor throws exception if diversityWhereRiseHappens is less than 0.")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          0.0,
-                          1.0,
-                          -1.0 ), Contains("must be between 0.0 and 1.0 inclusive") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            0.1,  
+            0.2,
+            -.1 // diversityWhereRiseHappens
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
 TEST_CASE("Resident_StepUp Ctor throws exception if diverstiyWhereRiseHappens is greater than 1.0.")
 {   
-    REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          0.0,
-                          1.0,
-                          1.1), Contains("must be between 0.0 and 1.0 inclusive") );
+   REQUIRE_THROWS_WITH(
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            0.1,  
+            0.2,
+            1.1 // diversityWhereRiseHappens
+        ),
+        Contains("must be between 0.0 and 1.0 inclusive")
+    );
 }
 
 TEST_CASE("Resident_StepUp Ctor throws exception if"
@@ -169,44 +153,49 @@ TEST_CASE("Resident_StepUp Ctor throws exception if"
 {   
     REQUIRE_THROWS_WITH(
         Resident_StepUp(
-            1, 
+            0, 
             Color::green, 
-            5.0,
-            0.25,
-            .75,
-            .25,
-            0.5), 
-        Contains("happinessAtZeroDiversity must be smaller than happinessAtOneDiversity.") );
+            0,
+            0,
+            .75, //happinessValueAtZeroDiversity
+            .25, //happinessValueAtOneDiversity
+            0), 
+        Contains("happinessAtZeroDiversity must be smaller than"
+            " happinessAtOneDiversity.")
+    );
 }
 
 TEST_CASE("Resident_StepUp Ctor throws exception if"
 " happinessValueAtZeroDiversity is the same as happinessValueAtOneDiversity.")
 {   
     REQUIRE_THROWS_WITH(
-        Resident_StepUp(1, 
-                          Color::green, 
-                          5.0,
-                          0.25,
-                          .75,
-                          .75,
-                          0.5), Contains("happinessAtZeroDiversity must be"
-                          " smaller than happinessAtOneDiversity.") );
+        Resident_StepUp(
+            0, 
+            Color::green, 
+            0,
+            0,
+            .75, // happinessValueAtZeroDiversity
+            .75, // happinessValueAtOneDiversity
+            0), 
+        Contains("happinessAtZeroDiversity must be smaller than"
+            " happinessAtOneDiversity.")
+    );
 }
-
 
 TEST_CASE("getHappines() returns higher happinessValue at point where rise happens.")
 {   
-    // Note diversity Ratio is 0.5. @diverstiyWhereRiseHappens is 0.50.
+    // Note @diverstiyWhereRiseHappens is 0.50.
     Resident_StepUp greenResident{ 
-        1, 
+        0, 
         Color::green, 
-        0.0,  // allowedMovementDistance (doesn't matter)
-        0.25, // happinessGoal (doesn't matter)
+        0.0,  
+        0.25,
         0.25, // happinessValueAtZeroDiversity
         0.75, // happinessValueAtOneDiversity
-        0.50   // diversityWhereDropHappens
+        0.50  // diversityWhereRiseHappens
     };
 
+    // diversity Ratio is 0.5.
     std::set<Resident*> neighbors = { 
         &grNeighbor01,
         &grNeighbor02,
@@ -217,19 +206,20 @@ TEST_CASE("getHappines() returns higher happinessValue at point where rise happe
     REQUIRE(greenResident.calculateHappiness(neighbors, 4) == 0.75);
 }
 
-TEST_CASE("getHappines() returns lower happinessValue before diversity drop.")
+TEST_CASE("getHappines() returns lower happinessValue before diversity rise.")
 {
-    // Note diversity Ratio is 0.25. @diverstiyWhereDropHappens is 0.5.
+    // Note @diverstiyWhereRiseHappens is 0.5.
     Resident_StepUp blueResident{ 
-        1, 
+        0, 
         Color::blue, 
-        0.0,  // allowedMovementDistance (doesn't matter)
-        0.25, // happinessGoal (doesn't matter)
+        0.0,  
+        0.0, 
         0.25, // happinessValueAtZeroDiversity
         0.75, // happinessValueAtOneDiversity
-        0.5   // diversityWhereDropHappens
+        0.5   // diversityWhereRiseHappens
     };
 
+    // Diversity Ratio is 0.25. 
     std::set<Resident*> neighbors = { 
         &blNeighbor01,
         &blNeighbor02,
@@ -240,19 +230,20 @@ TEST_CASE("getHappines() returns lower happinessValue before diversity drop.")
     REQUIRE(blueResident.calculateHappiness(neighbors, 4) == 0.25);
 }
 
-TEST_CASE("getHappines() returns higher happinessValue afterDiversity drop.")
+TEST_CASE("getHappiness() returns higher happinessValue afterDiversity rise.")
 {
-    // Note diversity Ratio is 0.75. @diverstiyWhereDropHappens is 0.25.
+    // Note @diverstiyWhereDropHappens is 0.5.
     Resident_StepUp blueResident{ 
-        1, 
+        0, 
         Color::blue, 
-        0.0,  // allowedMovementDistance (doesn't matter)
-        0.25, // happinessGoal (doesn't matter)
+        0.0, 
+        0.0,
         0.25, // happinessValueAtZeroDiversity
         0.75, // happinessValueAtOneDiversity
-        0.5   // diversityWhereDropHappens
+        0.5   // diversityWhereRiseHappens
     };
 
+    // Diversity Ratio is 0.75.
     std::set<Resident*> neighbors = { 
         &blNeighbor01,
         &grNeighbor01,
@@ -268,11 +259,11 @@ TEST_CASE("Resident_StepUp toStrBasic()")
     Resident_StepUp greenResident{ 
         1, 
         Color::green, 
-        0.0,  // allowedMovementDistance (doesn't matter)
-        0.25, // happinessGoal (doesn't matter)
+        0.0,  // allowedMovementDistance 
+        0.25, // happinessGoal
         0.25, // happinessValueAtZeroDiversity
         0.75, // happinessValueAtOneDiversity
-        0.5   // diversityWhereDropHappens
+        0.5   // diversityWhereRiseHappens
     };
     REQUIRE(greenResident.toStrBasic() == 
         "Res_StepUp(id, clr, mov, goal, hap0, hap1, rise):"
