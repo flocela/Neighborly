7aef58627c84befc6e5a63e52155667fb91737ea
Flo <flocela@gmail.com>
2023-05-24 20:57:02 +0000

    Added operator< test to test_house.cpp

--- tests/test_house.cpp
+++ tests/test_house.cpp
@@ -10,3 +10,10 @@ TEST_CASE("House toStr ()")
     House house (12);
     REQUIRE( house.toStr() == "House (addr): (12)");
 }
+
+TEST_CASE("operator<")
+{
+    House h1(1);
+    House h10(10);
+    REQUIRE( (h1 < h10) == true);
+}
