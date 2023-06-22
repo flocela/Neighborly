447cb1e1dc9807f83651fc346456079f59867510
Flo <flocela@gmail.com>
2023-06-22 23:42:25 +0000

    UI.h added comment to getAnswer(Question&& question)

--- src/UI.h
+++ src/UI.h
@@ -19,8 +19,8 @@ protected:
 public:
     virtual ~UI () noexcept = default;
 
-    // presents the user with a menu of choices and returns the user's response.
-    // if user does not reply with an number corresponding to an item, then returns the fallback item.
+    // Presents the user with a menu of choices and returns the user's response.
+    // If user does not reply with an number corresponding to an item, then returns the fallback item.
     virtual int menu (
         std::string prompt,
         std::vector<std::string> items,
@@ -28,12 +28,14 @@ public:
         std::string failureResponse
     ) const = 0;
 
-    // presents the user with the question and returns the user's response.
-    // if can not get a proper answer from the user, then the question's fallback answer
+    // Presents the user with the question and returns the user's response.
+    // If can not get a proper answer from the user, then the question's fallback answer
     // is returned.
     virtual std::string getAnswer (Question& question)const = 0;
 
-    // TODO write commment
+    // Presents the user with the question and returns the user's response.
+    // If can not get a proper answer from the user, then the question's fallback answer
+    // is returned.
     virtual std::string getAnswer (Question&& question) const = 0;
 
     };
